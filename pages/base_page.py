6928403681440f1909ab8cc42bee051ac47282a9
Vyacheslav V. Tomilin <glaurungh@gmail.com>
2021-11-01 18:18:44 +0300

    1. Tests test_guest_can_go_to_login_page() and test_guest_should_see_login_link() was joned in TestLoginFromMainPage class 2. This class was marked with login_guest mark 3. Added file pytest.ini with this mark description

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from .locators import BasePageLocators
 
 
-class BasePage():
+class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
