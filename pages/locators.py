f91203fc9475ca0e551eda7ccd08484619e872b5
Vyacheslav V. Tomilin <glaurungh@gmail.com>
2021-11-01 15:02:53 +0300

    Added tests of price and product name checking while adding product to basket

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,7 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form>button")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main>h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.col-sm-6.product_main>p.price_color")
+    PRODUCT_NAME_IN_MESSAGE = (By.CSS_SELECTOR, "#messages>div:nth-child(1)>div>strong")
+    PRODUCT_PRICE_IN_MESSAGE = (By.CSS_SELECTOR, "#messages>div.alert.alert-safe.alert-noicon.alert-info.fade.in>div>p:nth-child(1)>strong")
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,9 +1,28 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
+
 class ProductPage(BasePage):
 
     def test_guest_can_add_product_to_basket(self):
+        product_name = self.get_product_name()
+        product_price = self.get_product_price()
         add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_button.click()
         self.solve_quiz_and_get_code()
+        self.product_name_should_be_in_add_to_basket_messages(product_name)
+        self.product_price_should_be_in_add_to_basket_messages(product_price)
+
+    def product_name_should_be_in_add_to_basket_messages(self, product_name):
+        product_name_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE).text
+        assert product_name_in_message == product_name, f"Incorrect product name in basket: {product_name_in_message}"
+
+    def product_price_should_be_in_add_to_basket_messages(self, product_price):
+        product_price_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_MESSAGE).text
+        assert product_price_in_message == product_price, f"Incorrect product price in basket: {product_price_in_message}"
+
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
